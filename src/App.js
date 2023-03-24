f1225d33540aef58cf9473092d1949314e7b60c1
Jonathan Hawes <jhawes580@gmail.com>
2023-03-24 13:49:53 -0700

    Add routing basics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import Bookmarks from './components/Bookmarks';
+import Notifications from './components/Notifications';
+import Explore from './components/Explore';
+import Home from './components/Home';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={SignIn}></Route>
+        <Route path="/home" element={Home}></Route>
+        <Route path="/explore" element={Explore}></Route>
+        <Route path="/notifications" element={Notifications}></Route>
+        <Route path="/bookmarks" element={Bookmarks}></Route>
+        <Route path="/profile/:id"></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
