d07dcbca2e4737b35962d46a1dc397dd4ae717d9
Jonathan Hawes <jhawes580@gmail.com>
2023-03-24 13:44:03 -0700

    Add my own boilerplate for the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import './styles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
