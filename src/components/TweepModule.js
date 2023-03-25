8fd1c46c1c166ba2d08e8f515d04d0269d0b0999
Jonathan Hawes <jhawes580@gmail.com>
2023-03-25 15:39:31 -0700

    Add tweep module component

---
+++ src/components/TweepModule.js
@@ -0,0 +1,14 @@
+import Pic from '../assets/fakepic.jpeg';
+import '../styles/TweepModule.css';
+
+export default function TweepModule() {
+  return (
+    <div className="tweepModule">
+      <div className="tweepWriting"></div>
+      <div className="toolbar">
+        <div className="icons"></div>
+        <button className="tweepButton"></button>
+      </div>
+    </div>
+  );
+}
