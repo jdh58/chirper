2631ccc4f334a43578879d59a896fce154ebfb2d
Jonathan Hawes <jhawes580@gmail.com>
2023-03-24 17:21:25 -0700

    Add dynamic placeholder and hover transitions

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,11 +2,26 @@ import '../styles/SignIn.css';
 import Logo from '../assets/logo.svg';
 import Google from '../assets/google-original.svg';
 import Apple from '../assets/apple-original.svg';
+import Close from '../assets/close.svg';
+import { useState } from 'react';
 
 export default function SignIn() {
+  const [userInput, setUserInput] = useState('');
+
+  const checkUserInput = (e) => {
+    const inputVal = e.target.value;
+
+    if (inputVal.length > 0) {
+      setUserInput('active');
+    } else {
+      setUserInput('');
+    }
+  };
+
   return (
     <div className="signInOverlay">
       <div className="signInPopUp">
+        <img src={Close} alt="" className="close" />
         <img src={Logo} alt="" className="logo" />
         <h1 className="title">Sign in to Tweeper</h1>
         <div className="otherSignIn">
@@ -22,8 +37,13 @@ export default function SignIn() {
         <div className="separator">
           <p>or</p>
         </div>
-        <input type="text" name="username" id="username" />
-        {/* <div className="placeholder">Email or username</div> */}
+        <input
+          type="text"
+          name="username"
+          id="username"
+          onChange={checkUserInput}
+        />
+        <div className={`placeholder ${userInput}`}>Email or username</div>
         <div className="otherSignIn">
           <button className="next">Next</button>
           <button className="forgot">Forgot password?</button>
