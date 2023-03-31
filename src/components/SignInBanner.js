a4a818c9eaaa1fefb8f099412090e3509ff80017
Jonathan Hawes <jhawes580@gmail.com>
2023-03-31 00:52:56 -0700

    Style sign in banner

--- src/components/SignInBanner.js
+++ src/components/SignInBanner.js
@@ -1,5 +1,16 @@
 import '../styles/SignInBanner.css';
 
 export default function SignInBanner() {
-  return;
+  return (
+    <div className="signInBanner">
+      <div className="text">
+        <p className="top">Don't miss what's happening</p>
+        <p className="bottom">People on Chirper are the first to know.</p>
+      </div>
+      <div className="buttons">
+        <button className="logIn">Log in</button>
+        <button className="signUp">Sign up</button>
+      </div>
+    </div>
+  );
 }
