1190cc9be3d74bf4d03e8088162f8c3b7fb1aa15
Jonathan Hawes <jhawes580@gmail.com>
2023-04-06 00:01:26 -0700

    Improve more menu styling and logic for accounts

--- src/components/MoreMenu.js
+++ src/components/MoreMenu.js
@@ -1,16 +1,29 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import UserContext from '../UserContext';
 import Delete from '../assets/delete.svg';
 import Bookmark from '../assets/bookmark.svg';
 import '../styles/MoreMenu.css';
 
 export default function MoreMenu({ chirpData }) {
-  const user = useContext(UserContext);
+  const [isUsers, setIsUsers] = useState(false);
+
+  const user = useContext(UserContext) || {
+    userId: '',
+  };
+
+  // Detect if the chirp is the user's, so we know if we show delete
+  useEffect(() => {
+    if (user.userId === chirpData.accountId) {
+      setIsUsers(true);
+    }
+  }, []);
+
+  const handleDelete = () => {};
 
   return (
-    <div className="moreMenu more">
-      {user.userId === chirpData.accountId ? (
-        <div className="delete option">
+    <div className={isUsers ? 'moreMenu users' : 'moreMenu'}>
+      {isUsers ? (
+        <div className="delete option" onClick={handleDelete}>
           <img src={Delete} alt="" />
           <p>Delete</p>
         </div>
