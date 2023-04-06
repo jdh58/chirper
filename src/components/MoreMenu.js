594a606b8df857f66856454036aeed056c6e478c
Jonathan Hawes <jhawes580@gmail.com>
2023-04-06 02:15:23 -0700

    Add bookmarking logic

--- src/components/MoreMenu.js
+++ src/components/MoreMenu.js
@@ -11,6 +11,8 @@ import {
   where,
   deleteDoc,
   doc,
+  updateDoc,
+  arrayUnion,
 } from 'firebase/firestore';
 import { app } from '../firebase-config';
 
@@ -38,15 +40,42 @@ export default function MoreMenu({ chirpData, killMenu }) {
         )
       );
 
-      console.log(chirpToDelete.docs[0].data());
-
       await deleteDoc(chirpToDelete.docs[0].ref);
 
-      // Lower chirp count by 1
+      // Lower chirp count by 1... o
 
       // Display toast notification, refactor it to context over prop
     } catch (error) {
       console.error('Failed to delete Chirp', error);
+    }
+  };
+
+  const handleBookmark = async () => {
+    try {
+      killMenu();
+      const chirpToBookmark = await getDocs(
+        query(
+          collection(getFirestore(app), 'chirps'),
+          where('chirpId', '==', chirpData.chirpId)
+        )
+      );
+
+      const chirpToBookmarkData = chirpToBookmark.docs[0].data();
+
+      const accountToAddBookmark = await getDocs(
+        query(
+          collection(getFirestore(app), 'accounts'),
+          where('userId', '==', `${chirpData.accountId}`)
+        )
+      );
+
+      await updateDoc(accountToAddBookmark.docs[0].ref, {
+        bookmarks: arrayUnion(chirpToBookmarkData),
+      });
+
+      // Display toast notification here
+    } catch (error) {
+      console.error('Failed to bookmark Chirp.', error);
     }
   };
 
@@ -58,7 +87,7 @@ export default function MoreMenu({ chirpData, killMenu }) {
           <p>Delete</p>
         </div>
       ) : null}
-      <div className="bookmark option">
+      <div className="bookmark option" onClick={handleBookmark}>
         <img src={Bookmark} alt="" />
         <p>Bookmark</p>
       </div>
