c2d6f5796274b3580bef964fa5aedb40fea80b6d
Jonathan Hawes <jhawes580@gmail.com>
2023-04-19 20:31:59 -0700

    Add logic to grab following chirps.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import '../styles/Home.css';
 import '../styles/page.css';
 import Tab from './Tab';
@@ -6,10 +6,53 @@ import RightBar from './RightBar';
 import ChirpModule from './ChirpModule';
 import Chirp from './Chirp';
 import UserContext from '../UserContext';
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore';
+import { app } from '../firebase-config';
 
 export default function Home() {
   const user = useContext(UserContext);
   const [currentTab, setCurrentTab] = useState('foryou');
+  const [followingChirps, setFollowingChirps] = useState([]);
+
+  useEffect(() => {
+    console.log(user);
+    if (user) {
+      (async () => {
+        try {
+          const followChirpDocs = await getDocs(
+            query(
+              collection(getFirestore(app), 'chirps'),
+              where('accountId', 'in', user.following),
+              orderBy('postTime', 'desc'),
+              limit(10)
+            )
+          );
+
+          const followChirpsArray = [];
+
+          followChirpDocs.docs.forEach((chirpDoc) => {
+            const chirpData = chirpDoc.data();
+
+            followChirpsArray.push(
+              <Chirp chirpData={chirpData} key={chirpData.chirpId} />
+            );
+          });
+
+          setFollowingChirps(followChirpsArray);
+        } catch (error) {
+          console.error('Could not fetch following chirps', error);
+        }
+      })();
+    }
+  }, [user]);
 
   const setTab = (e) => {
     setCurrentTab(e.currentTarget.classList[0]);
@@ -35,6 +78,7 @@ export default function Home() {
           <div className={`indicator ${currentTab}`}></div>
         </header>
         <ChirpModule isReply={false} />
+        {currentTab === 'foryou' ? followingChirps : null}
       </div>
       <RightBar />
     </>
