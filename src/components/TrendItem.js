2fc244c42963162d91a9993a3758368dab60419f
Jonathan Hawes <jhawes580@gmail.com>
2023-04-19 20:03:07 -0700

    Add trend module redirect and fix search subheader

--- src/components/TrendItem.js
+++ src/components/TrendItem.js
@@ -1,10 +1,18 @@
 import '../styles/TrendItem.css';
 import More from '../assets/more.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function TrendItem({ number, name, count }) {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className="trendItem">
+      <div
+        className="trendItem"
+        onClick={() => {
+          navigate(`/search/${name.slice(1)}`);
+        }}
+      >
         <div className="trendInfo">
           <span className="title">
             <p className="number">{number}</p>
